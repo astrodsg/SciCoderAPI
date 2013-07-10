5887fca0a4eb59e5573558898102761ef88e4045
astrodsg_laptop <gregersen.dylan@gmail.com>
2013-07-10 17:11:17 -0400

    Making the function for the SDSS but simply just have the x and y

--- plotting.py
+++ plotting.py
@@ -9,12 +9,22 @@ import matplotlib.pylab as plt
 #=======================================================================#
 #
 
-def HistPlot (fitstable,column1,column2):
+def HistPlot (x,y,binsize=10)
+
 	# initialize figure and axes
 	fig = plt.figure()
 	axes = fig.add_subplot(111)
-	
-	# parameters
 
-	pass
+	# initialize some parameters for the plot
+	
+	# the x bounds
+	xmin = np.min(x)
+	xmax = np.max(x)
+
+	# the bin edges based on the bin size
+	bins = np.arange(xmin,xmax,binsize)
+	
+	# plot parameters
+	axes.set_xlim(xmin,xmax)
+
 	
