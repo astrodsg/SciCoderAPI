d271d0dbf91d36068e5a3a241bcb36271d861a57
astrodsg_laptop <gregersen.dylan@gmail.com>
2013-07-10 17:05:48 -0400

    Created the initial file for plotting routines

---
+++ plotting.py
@@ -0,0 +1,20 @@
+
+#=======================================================================#
+
+import numpy as np
+from .utilities import FitsTable
+import matplotlib.pylab as plt
+
+
+#=======================================================================#
+#
+
+def HistPlot (fitstable,column1,column2):
+	# initialize figure and axes
+	fig = plt.figure()
+	axes = fig.add_subplot(111)
+	
+	# parameters
+
+	pass
+	
