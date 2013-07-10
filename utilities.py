cd9f6dedfd45489bc08c14363df9a7c499e5fffe
astrodsg_laptop <gregersen.dylan@gmail.com>
2013-07-10 16:47:01 -0400

    edited the file

---
+++ utilities.py
@@ -0,0 +1,66 @@
+import os
+from astropy.io import fits
+import numpy as np
+import sys
+import glob
+
+
+class FitsTable (object):
+
+    def __init__ (self, filepath, fix_nan=None):
+
+        # check that the file exists
+        if not os.path.isfile(filepath):
+            raise IOError("File does not exist :"+str(filepath))
+
+        # open the fits file
+        self.hdulist = fits.open(self.filepath, memmap=False)
+
+        # filepath
+        self.filepath = os.path.abspath(self.filepath)
+
+        # table header
+        self.header = self.hdulist[1].header
+
+        # store the nan value you want to fix
+        self.fix_nan = fix_nan
+
+        # initialize the columns and numbers
+        self.columns = self.hdulist[1].columns.names
+
+    def _fix_nan_values (self,arr):
+        if self.fix_nan is None: return arr
+        arr[arr == self.fix_nan] = np.NaN
+        return arr
+            
+    def __getitem__ (self,column):
+        """ Accessing columns"""
+        # get the array
+        # fix the nan values
+        # return the array
+        pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# get the entire header 
+# keyword == TTYPE*
+# keyword get the column number
+# get the column label
+
+        
+        
