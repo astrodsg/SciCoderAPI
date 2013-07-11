bf8f238409b65f4b8ba3dd81b436b1c61c566d2d
Ivelina Momcheva <selene@selene.local>
2013-07-11 14:24:26 -0400

    Added getitem functionality. Untested.

--- utilities.py
+++ utilities.py
@@ -7,7 +7,7 @@ import glob
 
 class FitsTable (object):
 	
-    def __init__ (self, filepath, fix_nan=None):
+    def __init__(self, filepath, fix_nan=None):
 		
         # check that the file exists
         if not os.path.isfile(filepath):
@@ -19,8 +19,6 @@ class FitsTable (object):
         # open the fits file
         self.hdulist = fits.open(self.filepath, memmap=False)
 
-     
-
         # table header
         self.header = self.hdulist[1].header
 
@@ -30,17 +28,16 @@ class FitsTable (object):
         # initialize the columns and numbers
         self.columns = self.hdulist[1].columns.names
 
-    def _fix_nan_values (self,arr):
+    def _fix_nan_values(self,arr):
         if self.fix_nan is None: return arr
         arr[arr == self.fix_nan] = np.NaN
         return arr
             
-    def __getitem__ (self,column):
+    def __getitem__(self,column):
         """ Accessing columns"""
-        # get the array
-        # fix the nan values
-        # return the array
-        pass
+
+        if column in self.columns and type(column) == str:
+        	return np.array(self.hdulist[1].data.field(column))
 
 
 
